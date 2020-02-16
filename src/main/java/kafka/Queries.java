03e97eaec6c3b3ad9bdbb290f66fb48a657f32fe
daos <daos@me.com>
2020-02-16 10:10:59 +0100

    satoshi to btc

--- src/main/java/kafka/Queries.java
+++ src/main/java/kafka/Queries.java
@@ -1,23 +1,24 @@
 package kafka;
 
 import java.time.Instant;
+import java.util.Arrays;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Windowed;
 import org.apache.kafka.streams.state.*;
-
+import java.util.stream.Stream;
 import java.util.ArrayList;
 
 public class Queries {
 
   private KafkaStreams streams;
 
-  public class Item<K, V> {
+  public static class Item<K, V> {
 
     public K key;
     public V value;
 
-    Item(K k, V v) {
+    public Item(K k, V v) {
       key = k;
       value = v;
     }
@@ -27,16 +28,21 @@ public class Queries {
     this.streams = streams;
   }
 
-  public ArrayList<Item> queryAllDays(String storeName) {
+  public ArrayList<Item<Long,Long>> queryAllDays(String storeName) {
     final ReadOnlyKeyValueStore<Long, Long> store =
         streams.store(storeName, QueryableStoreTypes.keyValueStore());
-    ArrayList<Item> res = new ArrayList<>();
+    ArrayList<Item<Long,Long>> res = new ArrayList<>();
     KeyValueIterator<Long, Long> iter = store.all();
     while (iter.hasNext()) {
       KeyValue<Long, Long> next = iter.next();
-      res.add(new Item<>(next.key, next.value));
+      res.add(new Item<>(next.key, (long)(Math.abs(next.value)/100000000f)));
     }
     return res;
+  }
+
+  public Stream<Item<Long,Long>> streamAllDays(){
+    ArrayList<Item<Long,Long>> result= queryAllDays("bitcoin-aggregates-allday");
+    return result.stream();
   }
 
   public ArrayList<Item> queryAddresses(String storeName, long start, long end) {
